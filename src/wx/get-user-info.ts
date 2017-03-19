03001279cf52e41b34194110cc49a1e0c77c548c
taoqf <tao_qiufeng@126.com>
2017-03-19 17:18:12 +0800

    change token

--- src/wx/get-user-info.ts
+++ src/wx/get-user-info.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { https } from '../get';
 import config from '../config';
+import { get_token } from './token';
 
 export default (req: Request, res: Response, next: NextFunction) => {
 	const code = req.body.code || req.query.code;
@@ -10,7 +11,7 @@ export default (req: Request, res: Response, next: NextFunction) => {
 	}
 	console.log('geting userinfo: code=', code);
 	https(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${config.APPID}&secret=${config.APPSECRET}&code=${code}&grant_type=authorization_code`).then((data) => {
-		console.log('access_token:', data);
+		console.log('openid:::', data);
 		const d = JSON.parse(data);
 		const info = d as {
 			access_token: string;
@@ -21,40 +22,46 @@ export default (req: Request, res: Response, next: NextFunction) => {
 			unionid: string;
 		};
 		if (info.openid && info.access_token) {
-			https(`https://api.weixin.qq.com/cgi-bin/user/info?access_token=${info.access_token}&openid=${info.openid}&lang=zh_CN`).then((data) => {
-				console.log('userinfo:::', data);
-				const d = JSON.parse(data);
-				const info = d as {
-					subscribe: number;
-					openid: string;
-					nickname: string;
-					sex: number;
-					language: string;
-					city: string;
-					province: string;
-					country: string;
-					headimgurl: string;
-					subscribe_time: number;
-					unionid: string;
-					remark: string;
-					groupid: number;
-				};
-				if (info.subscribe) {
-					res.json(info);
-				} else {
-					if (info.subscribe === 0) {
-						res.status(500).end('此用户没有关注该公众号');
+			get_token().then((token) => {
+				console.log('token getted:', token);
+				https(`https://api.weixin.qq.com/cgi-bin/user/info?access_token=${token}&openid=${info.openid}&lang=zh_CN`).then((data) => {
+					console.log('userinfo:::', data);
+					const d = JSON.parse(data);
+					const info = d as {
+						subscribe: number;
+						openid: string;
+						nickname: string;
+						sex: number;
+						language: string;
+						city: string;
+						province: string;
+						country: string;
+						headimgurl: string;
+						subscribe_time: number;
+						unionid: string;
+						remark: string;
+						groupid: number;
+					};
+					if (info.subscribe) {
+						res.json(info);
 					} else {
-						const e = d as {
-							errcode: number;
-							errmsg: string;
-						};
-						console.error('Exception when get userinfo, detail:', e.errmsg);
-						res.status(500).end(e.errmsg);
+						if (info.subscribe === 0) {
+							res.status(500).end('此用户没有关注该公众号');
+						} else {
+							const e = d as {
+								errcode: number;
+								errmsg: string;
+							};
+							console.error('Exception when get userinfo, detail:', e.errmsg);
+							res.status(500).end(e.errmsg);
+						}
 					}
-				}
+				}, (err) => {
+					res.status(500).end('Error: cannot get userinfo.');
+				});
+
 			}, (err) => {
-				res.status(500).end('Error: cannot get userinfo.');
+				res.status(500).end('cannot get token');
 			});
 		} else {
 			const e = d as {
