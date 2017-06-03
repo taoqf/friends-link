0cc05c7f1ac1debae20fab93c63b2a22fcd47480
taoqf <tao_qiufeng@126.com>
2017-06-03 12:08:10 +0800

    add retry in getting token

--- src/wx/token.ts
+++ src/wx/token.ts
@@ -1,39 +1,49 @@
-import config from '../config';
 import { https } from '../get';
+import * as querystring from 'querystring';
+import config from '../config';
+
+const retry_delay = 20 * 1000;
 let token: string;
-function refresh() {
+
+async function refresh() {
 	console.log('refreshing token.');
-	return https(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${config.APPID}&secret=${config.APPSECRET}`).then((data) => {
-		try {
-			const parsedData = JSON.parse(data) as {
-				access_token: string;
-				expires_in: number;
-				errcode: number;
-				errmsg: string;
-			};
-			console.log('token==============', parsedData);
-			console.info(parsedData);
-			if (parsedData.access_token) {
-				token = parsedData.access_token;
-				if (parsedData.expires_in > 0) {
-					const timeout = parsedData.expires_in * 1000 - 10000;
-					setTimeout(refresh, timeout);
-				}
-				return token;
-			} else {
-				return Promise.reject(parsedData);
+	try {
+		const query = querystring.stringify({
+			grant_type: 'client_credential',
+			appid: config.APPID,
+			secret: config.APPSECRET
+		});
+		const data = await https(`https://api.weixin.qq.com/cgi-bin/token?${query}`);
+		const parsedData = JSON.parse(data) as {
+			access_token: string;
+			expires_in: number;
+			errcode: number;
+			errmsg: string;
+		};
+		console.log('token==============', parsedData);
+		console.info(parsedData);
+		if (parsedData.access_token) {
+			token = parsedData.access_token;
+			if (parsedData.expires_in > 0) {
+				const timeout = parsedData.expires_in * 1000 - 10000;
+				setTimeout(refresh, timeout);
 			}
-		} catch (e) {
-			console.log(e.message);
-			return Promise.reject(e.message);
+			return token;
+		} else {
+			setTimeout(refresh, retry_delay);
+			throw parsedData;
 		}
-	});
+	} catch (e) {
+		console.log(e.message);
+		setTimeout(refresh, retry_delay);
+		throw e;
+	}
 }
 
-export function get_token() {
+export async function get_token() {
 	if (token) {
-		return Promise.resolve(token);
+		return token;
 	} else {
-		return refresh();
+		return await refresh();
 	}
 }
